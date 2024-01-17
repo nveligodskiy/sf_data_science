96d4e1c2e2e1dae22bcb5ff3e6b90525c55779ef
nveligodskiy <nikita.veligodskiy@gmail.com>
2024-01-17 20:35:26 +0100

    add game

---
+++ game_v2_Veligodskiy.py
@@ -0,0 +1,61 @@
+import numpy as np
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+    count = 0
+    predict = np.random.randint(1, 101)
+    
+    if predict>number:
+        number=int(number*2)
+    else:
+        number=int(number/2)
+
+    if predict>number:
+        number+=12
+    else:
+        number=number-12
+
+    if predict>number:
+        number+=7
+    else:
+        number=number-7
+    
+    if predict>number:
+        number+=3
+    else:
+        number=number-3
+    
+    while number!=predict:
+        count+=1            
+        if number >= predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+    # Ваш код заканчивается здесь
+
+    return count
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
